4490bdd2fd8787db50e4c654c9c0eb9b0958cbac
Brenda Monteiro <brenda.monteiro.melo@gmail.com>
2020-02-19 12:18:52 -0300

    banco de dados adicionado, aplicaçao finalizada

---
+++ db.js
@@ -0,0 +1,10 @@
+
+pool = {
+    user: 'postgres',
+    password:'0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'donation'
+};
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const server = express();
+const pool = require('./db')
 
 //config template engine
 const nunjucks = require("nunjucks");
@@ -9,24 +10,7 @@ nunjucks.configure("./", {
 });
 
 
-const donors = [
-    {
-        name: "Brenda M",
-        blood:"O+"
-    },
-    {
-        name: "Deborah M",
-        blood:"O+"
-    },
-    {
-        name: "Edson M",
-        blood:"O+"
-    },
-    {
-        name: "Gustavo M",
-        blood:"O+"
-    }
-]
+
 
 //config server p/ mostrar arq. estaticos
 server.use(express.static('public'));
@@ -34,9 +18,22 @@ server.use(express.static('public'));
 //habilitar body do form
 server.use(express.urlencoded({extended: true}));
 
+//config conexão bd
+const Pool = require('pg').Pool;
+const db = new Pool(pool);
+
 //config apres da pagina
 server.get("/", function(req, res) {
-    return res.render("index.html", {donors});
+    
+    db.query("SELECT * FROM donors", function(err, result){
+        if (err) return res.send("Erro no banco select");
+
+
+        const donors = result.rows;
+        return res.render("index.html", {donors});
+    });
+
+    
 });
 
 server.post("/", function(req, res){
@@ -45,12 +42,24 @@ server.post("/", function(req, res){
     const email = req.body.email;
     const blood = req.body.blood;
     
-    donors.push({
-        name: name,
-        blood: blood,
+if (name == "" || email == "" || blood == ""){
+    return res.send("Todos os campos são obrigatórios")
+}
+
+    //colocar valores no bd
+    const query = `INSERT INTO donors ("name", "email", "blood") 
+    VALUES ($1, $2, $3)`;
+    const values = [name, email, blood];
+
+    //tratamento de erro
+    db.query(query, values, function(err){
+        if (err) return res.send("Erro no banco de dados");
+
+        //ideal
+        return res.redirect("/");
     });
 
-    return res.redirect("/");
+    
 });
 
 server.listen(3001);
